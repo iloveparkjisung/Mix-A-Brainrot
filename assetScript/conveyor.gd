7fb5e5145f072ea763d1d789bb497ac6b6ed2f56
Aryanne <aryannemondeja@riccarton.school.nz>
2026-07-01 10:28:41 +1200

    I managed to make it spawn in my eggs
    I will need to edit it so its not half way in the ground spawning and not bouncing at eachother.

--- assetScript/conveyor.gd
+++ assetScript/conveyor.gd
@@ -1,10 +1,22 @@
 extends Node3D
 @export var speed := 2.0
-
-
+@export var spawn_delay := 2.0
+@export var egg_scene: PackedScene
+@onready var path: Path3D = $Path3D
 func _process(delta) -> void:
 	for child in $Path3D.get_children():
 		if child is PathFollow3D:
 			child.progress += speed * delta
 	
-	pass
+func _ready():
+	spawn_loop()
+
+func spawn_loop():
+	while true:
+		spawn_egg()
+		await get_tree().create_timer(spawn_delay).timeout
+
+func spawn_egg():
+	var egg = egg_scene.instantiate()
+	egg.progress_ratio = 0.0
+	path.add_child(egg)
--- assetScript/egg.gd
+++ assetScript/egg.gd
@@ -2,4 +2,7 @@ extends PathFollow3D
 @export var speed := 2.0
 
 func _process(delta: float) -> void:
+	progress += speed * delta
 	
+	if progress_ratio >= 1.0:
+		queue_free()
