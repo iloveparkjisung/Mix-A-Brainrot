b1aba88d717702effaa338b7dd91820afc79de37
Aryanne <aryannemondeja@riccarton.school.nz>
2026-07-22 10:11:24 +1200

    egg pick up! player knows it can pick up egg!
    I have made it possible for the player to pick up the egg and hold it in front of them!

--- assetScript/egg.gd
+++ assetScript/egg.gd
@@ -1,6 +1,7 @@
 extends PathFollow3D
 @export var speed := 2.0
 
+
 func _process(delta: float) -> void:
 	progress += speed * delta
 	
--- assetScript/eggbody.gd
+++ assetScript/eggbody.gd
@@ -1,5 +1,14 @@
 extends RigidBody3D
-
+var held = false
 
 func _ready() -> void:
 	%EggBody.freeze = true
+
+
+func interact():
+	print("egg")
+	
+	var player = get_tree().get_first_node_in_group("player")
+	
+	if player:
+		player.pick_up(self)
