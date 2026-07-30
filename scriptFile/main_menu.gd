cb913792cf735b450e49dbdd43c7eb6417e5b773
Aryanne <aryannemondeja@riccarton.school.nz>
2026-07-31 00:09:17 +1200

    Quit button main menu
    I made it so when you press the quit button it quits!!

--- scriptFile/main_menu.gd
+++ scriptFile/main_menu.gd
@@ -13,3 +13,7 @@ func _process(delta: float) -> void:
 
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://sceneFile/main_level.tscn")
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
