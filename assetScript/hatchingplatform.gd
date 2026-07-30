a961e0dfc18dfc51c664e9e908ab046e5ff422ca
Aryanne <aryannemondeja@riccarton.school.nz>
2026-07-30 22:24:57 +1200

    Create hatchingplatform.gd

---
+++ assetScript/hatchingplatform.gd
@@ -0,0 +1,15 @@
+extends Node3D
+@onready var egg_point = $EggPoint
+var egg = null
+
+func place_egg(new_egg):
+	if egg != null:
+		return false
+	
+	egg = new_egg
+	egg.reparent(self)
+	
+	egg.global_position = egg_point.global_position
+	egg.global_rotation = egg_point.global_rotation
+	
+	return true
